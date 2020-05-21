debab77e79a96dfe14e0bd12c038bf8013259994
RicardoYamartin <150300804@ucaribe.edu.mx>
2020-05-20 21:34:53 -0500

    SUBIDA DE IMAGENES

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,11 +1,26 @@
+const path = require('path');
+const { randomNumber } = require('../helpers/libs');
+const fs = require('fs-extra');
+
 const ctrl = {};
 
 ctrl.index = (req, res) => {
     
 };
 
-ctrl.create = (req, res) => {
-    
+ctrl.create = async (req, res) => {
+    const imgUrl = randomNumber();
+    console.log(imgUrl);
+    const imageTempPath = req.file.path;
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+
+    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+        // you wil need the public/temp path or this will throw an error
+        await fs.rename(imageTempPath, targetPath);
+    }
+    res.send('works');
+
 };
 
 ctrl.like = (req, res) => {

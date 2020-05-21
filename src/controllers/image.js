050e894f1930c8e6c2538a3cd24c405dcd237635
RicardoYamartin <150300804@ucaribe.edu.mx>
2020-05-21 02:36:54 -0500

    PUBLICAR COMENTARIOS

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const { randomNumber } = require('../helpers/libs');
 const fs = require('fs-extra');
-const { Image } = require('../models');
+const { Image,Comment } = require('../models');
+const md5 = require('md5');
 
 const ctrl = {};
 
@@ -47,8 +48,7 @@ ctrl.create = (req, res) => {
               description: req.body.description
             });
             const imageSaved = await newImg.save();
-            //res.redirect('/images/');
-            res.send('works');
+            res.redirect('/images/' + imageSaved.uniqueId);
           } else {
             await fs.unlink(imageTempPath);
             res.status(500).json({ error: 'Solo imagenes estan permitidas' });
@@ -62,8 +62,18 @@ ctrl.create = (req, res) => {
 ctrl.like = (req, res) => {
     
 };
-ctrl.comment = (req, res) => {
-    
+
+ctrl.comment= async (req, res) => {
+  const image = await Image.findOne({filename: {$regex: req.params.image_id}});
+  if (image) {
+    const newComment = new Comment(req.body);
+    newComment.gravatar = md5(newComment.email);
+    newComment.image_id = image._id;
+    await newComment.save();
+    res.redirect('/images/' + image.uniqueId /*+ '#' + newComment._id*/);
+  } else {
+    res.redirect('/');
+  }
 };
 
 ctrl.remove = (req, res) => {
---
+++ src/models/comment.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+const ObjectId = Schema.ObjectId;
+const path = require('path');
+
+const CommentSchema = new Schema({
+  image_id: { type: ObjectId },
+  email: { type: String },
+  name: { type: String },
+  gravatar: { type: String },
+  comment: { type: String },
+  timestamp: { type: Date, default: Date.now }
+});
+
+/*CommentSchema.virtual('image')
+  .set(function(image) {
+    this._image = image;
+  })
+  .get(function () {
+    return this._image;
+  });*/
+
+module.exports = model('Comment', CommentSchema);
